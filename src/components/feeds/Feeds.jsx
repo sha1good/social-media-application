f5fcd6aee425c0c2ccd5141a73da058db6339278
sha1good <bankoleyakub11@yahoo.com>
2022-10-30 15:39:19 +0100

    adding new link

--- src/components/feeds/Feeds.jsx
+++ src/components/feeds/Feeds.jsx
@@ -8,19 +8,20 @@ import axios from "axios";
 const Feeds = ({username}) => {
   const [posts, setPosts] = useState([]);
   const { user } = useContext(AuthContext);
+  const reactbaseUrl = process.env.REACT_APP_BASE_URL;
    
   useEffect(() => {
     const fetchPosts = async () => {
       const response = username ? 
-      await axios.get("/posts/profile/" + username) : 
-      await axios.get("/posts/timeline/" + user._id);
+      await axios.get(`${reactbaseUrl}/posts/profile/` + username) : 
+      await axios.get(`${reactbaseUrl}/posts/timeline/` + user._id);
      setPosts(response.data.sort((p1, p2) => {
           return new Date(p2.createdAt) - new Date(p1.createdAt);
         })
       );
     };
     fetchPosts();
-  }, [user._id,username]);
+  }, [user._id,username,reactbaseUrl]);
   return (
     <div className="feeds">
       <div className="feedWrapper">
